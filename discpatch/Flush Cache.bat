b922f75c7616bfda4b486fc41cfe11c96a9cba08
nastys <7950891+nastys@users.noreply.github.com>
2026-08-07 08:55:08 +0200

    Add alternative cache paths (#5)

--- discpatch/Flush Cache.bat
+++ discpatch/Flush Cache.bat
@@ -1,3 +1,5 @@
 del "%LOCALAPPDATA%\Xbox360BackwardCompatibil.PrimaryConkerLiveReloade\VirtualDrive\Harddisk\Cache0\RawPartition"
 del "%LOCALAPPDATA%\Xbox360BackwardCompatibil.PrimaryConkerLiveReloade\VirtualDrive\Harddisk\Cache1\RawPartition"
+del "%LOCALAPPDATA%\Packages\Xbox360BackwardCompatibil.PrimaryConkerLiveReloade_ksqcvrsvwz2jp\LocalCache\Local\VirtualDrive\Harddisk\Cache0\RawPartition"
+del "%LOCALAPPDATA%\Packages\Xbox360BackwardCompatibil.PrimaryConkerLiveReloade_ksqcvrsvwz2jp\LocalCache\Local\VirtualDrive\Harddisk\Cache1\RawPartition"
 @pause
